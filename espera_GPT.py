b6df5c4f619fb5224fbdf6d98f15007c64674d2a
rmcantini <rmcantini@gmail.com>
2023-04-27 14:25:41 -0300

    the correct and possibly final version

--- espera_GPT.py
+++ espera_GPT.py
@@ -50,24 +50,3 @@ def main():
 
 # Calls the thing
 main()
-
-"""
-In this version, I removed the unnecessary imports of tkinter
-and made some changes to the variable names to make them more
-readable. I also moved the format string into the strptime()
-call and removed the print() statement, since it didn't seem 
-ecessary. Finally, I changed the showinfo() call to messagebox
-showinfo() to be consistent with the import statement.
-
-
-This will round time_final to the nearest minute and display
-the rounded value in the messagebox.
-
-In this updated code, after the user inputs the worked_time,
-the code prompts the user to input the num_clients by calling
-simpledialog.askinteger(). The function sets a minimum value of
-1 and a maximum value of 100 for the input, but these values
-can be adjusted as needed. The time_final calculation then
-divides time_solo by the number of clients (num_clients)
-to get the wait time for each client.
-"""
